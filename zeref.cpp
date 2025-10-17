b03307bf0fc49d88c31bb8eda85708a4ee24841d
zeref habibeh <zeref.habibah@gmail.com>
2025-10-17 00:33:33 -0400

    testing

--- zeref.cpp
+++ zeref.cpp
@@ -1,8 +1,42 @@
 #include <iostream>
 #include "matchit.h"
+using namespace std
 
+enum tokens {
+	Identifier;
+	Constant;
+	Int_keyword; 
+	Void_keyword;
+	Return_key word;
+	Lparen;
+	Rparen;
+	Lbrace;
+	Rbrace;
+	semicolon;
+}
+vector<tokens> Tokenizer(string &sourcecode){
+
+	word = null;
+	for( char& ch : sourcecode){
+		if (*ch == " "){
+
+			ch++;
+		}
+		
+		else{
+
+
+		}
+	
+			
+
+
+	}		
+
+
+} 
 int main() {
-	std::cout << "hello world";
+		
 	return 0;
 	
 }
